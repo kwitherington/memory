be15139d366ee72d54936e5ac6e71038723653c5
KOV <43220938+kwitherington@users.noreply.github.com>
2019-03-12 22:07:56 -0500

    Updates

---
+++ precache-manifest.7e848ad31cfe8cc49a89eeaba246ea4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bba3517bfce1346f5e2a",
+    "url": "/memory/static/js/runtime~main.af9bcf6a.js"
+  },
+  {
+    "revision": "6c9adefcb89ffe95c269",
+    "url": "/memory/static/js/main.7f187dbb.chunk.js"
+  },
+  {
+    "revision": "ff661de6195320cfc0eb",
+    "url": "/memory/static/js/2.822769df.chunk.js"
+  },
+  {
+    "revision": "6c9adefcb89ffe95c269",
+    "url": "/memory/static/css/main.3d5ccc0f.chunk.css"
+  },
+  {
+    "revision": "34cea4cdcb767577d388fb4fdfd88ed4",
+    "url": "/memory/index.html"
+  }
+];
